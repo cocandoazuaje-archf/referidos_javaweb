224310c041ac125689d8b54091c010d1e8293466
carloseocandop-glitch <cocandoazuaje@gmail.com>
2026-04-25 16:58:51 -0400

    creando creando branch [prueba_superada]

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Módulo principal con dependencias
+ */
+var app = angular.module('app', []);
+app.controller('MainCtrl', function () {
+
+    var vm = this;
+    vm.nombre = "";
+    vm.mensaje = "Esperando...";
+
+    vm.saludar = function () {
+        alert("AngularJS está funcionando correctamente!");
+
+    };
+});
+
+
+
